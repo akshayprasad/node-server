f2f830e72228906365091885fb7e33f9a119ebb9
Akshay Prasad <akshpras@cisco.com>
2019-06-23 20:23:13 +0530

    Added server health check.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import { inversifyServer } from './core/inversify.restify';
 
 const app = inversifyServer.build();
 
+app.get('/', (req, res) => {
+    res.send('Server health check!');
+});
+
 app.listen(3001, () => {
     // tslint:disable-next-line:no-console
     console.log(`INFO: Node app 'REST-Server is running at ${app.router}`);
